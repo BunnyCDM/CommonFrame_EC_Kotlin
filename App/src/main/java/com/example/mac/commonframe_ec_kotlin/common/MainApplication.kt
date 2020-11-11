4839e24c49dbecb9b1ee82580ab9f80186fd8cce
bunny <chendongming@rsdalstar-rd.com>
2020-11-11 14:10:55 +0800

    ...

--- App/src/main/java/com/example/mac/commonframe_ec_kotlin/common/MainApplication.kt
+++ App/src/main/java/com/example/mac/commonframe_ec_kotlin/common/MainApplication.kt
@@ -1,5 +1,6 @@
 package com.example.mac.commonframe_ec_kotlin.common
 
+import android.support.multidex.MultiDex
 import cn.jpush.android.api.JPushInterface
 import com.example.baselibrary.common.BaseApplication
 
@@ -15,5 +16,7 @@ class MainApplication : BaseApplication() {
         //极光推送初始化
         JPushInterface.setDebugMode(true)
         JPushInterface.init(this)
+
+        MultiDex.install(this)
     }
 }
